16e3a91defa300d059cd1f47ff1736f2818c934e
Leela Tirumal Kotti <leelathirumalkotti@gmail.com>
2025-05-10 11:25:49 +0530

    thirdcommit

--- src/Components/CustomerDashboard.js
+++ src/Components/CustomerDashboard.js
@@ -2,7 +2,7 @@ import React, { use, useEffect, useState } from 'react'
 import { collection,getFirestore, getDocs, doc,addDoc,getDoc } from 'firebase/firestore';
 import {auth,db} from "./firebase"
 import { useNavigate } from 'react-router-dom';
-
+import CustomerOrders from './CustomerOrders'
 import './CustomerDashboard.css';
 function CustomerDashboard() {
 
@@ -15,6 +15,8 @@ function CustomerDashboard() {
   const[customerAddress,setCustomerAddress]=useState()
   const navigate = useNavigate();
   const [userdata,setUserData]=useState();
+
+  const [screen,setScreen]=useState("mainscreen");
 
   //userdata
           useEffect(() => {
@@ -108,7 +110,7 @@ function CustomerDashboard() {
               <input placeholder='EnterLocation' type='text' id='location' className='me-3'/>
               <input placeholder='Search' type="text" id='search'/>
             </div>
-            <button className='btn mx-auto' onClick={()=>{SetProfileToggle(true)}}> 
+            <button className='btn mx-auto' onClick={()=>{SetProfileToggle(profileToggle => !profileToggle)}}> 
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="white" class="bi bi-person-square" viewBox="0 0 16 16">
               <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
               <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
@@ -116,44 +118,54 @@ function CustomerDashboard() {
             </button>
       </div>
 
-      <div className="container mt-4">
-  <div className="row">
-    {cards.map((card, index) => (
-      <div className="col-12 col-sm-6 col-md-4 mb-4" key={index}>
-            <div className="card h-100">
-              <img
-                src={card.image} // your image path here
-                className="card-img-top"
-                alt="Card"
-                style={{ height: '180px', objectFit: 'cover' }}
-              />
-              <div className="card-body d-flex flex-column">
-                <h5 className="card-title">{card.FirstName}</h5>
-                <p className="card-text">{card.Location}</p>
-                <p className='card-text'>PhoneNumber:{card.PhoneNumber}</p>
-                <a href="#" className="btn btn-primary mt-auto" onClick={() => setSelectedCard(card)} >
-                  Book Now
-                </a>
-              </div>
+         { screen ==="mainscreen" && (  <div className="container mt-4">
+            <div className="row">
+              {cards.map((card, index) => (
+                <div className="col-12 col-sm-6 col-md-4 mb-4" key={index}>
+                      <div className="card h-100">
+                        <img
+                          src={card.image} // your image path here
+                          className="card-img-top"
+                          alt="Card"
+                          style={{ height: '180px', objectFit: 'cover' }}
+                        />
+                        <div className="card-body d-flex flex-column">
+                          <h5 className="card-title">{card.FirstName}</h5>
+                          <p className="card-text">{card.Location}</p>
+                          <p className='card-text'>PhoneNumber:{card.PhoneNumber}</p>
+                          <a href="#" className="btn btn-primary mt-auto" onClick={() => setSelectedCard(card)} >
+                            Book Now
+                          </a>
+                        </div>
+                      </div>
+                </div>
+              ))}
             </div>
-      </div>
-    ))}
-  </div>
-</div>
+          </div>
+          )
+        }
 
-
-
-
+    {
+      /*
+      ORDERS
+      */
+    }
+        {
+          screen ==="orders" &&(
+            <CustomerOrders/>
+          )
+        }
 
 
 
 
 {profileToggle &&(
-  <div className='position-absolute top-50 start-10'>
+  <div  className="position-absolute bg-white border rounded shadow p-3"
+      style={{ top: '10%', left: '80%', right:'0%',zIndex: 1000 }}>
       <div>
       <h4>{userdata.FirstName}</h4>
-      <button className='btn'>Orders</button><br/>
-      <button className='btn'>LogOut</button>
+      <button className='btn' onClick={()=>{setScreen("orders")}}>Orders</button><br/>
+      <button className='btn' onClick={()=>{navigate('/login')}}>LogOut</button>
       </div>
     </div>
 )
--- src/Components/CustomerOrders.js
+++ src/Components/CustomerOrders.js
@@ -3,7 +3,9 @@ import React from 'react'
 function CustomerOrders() {
   return (
     <>
-    
+    <div>
+      <h3>Orders</h3>
+    </div>
     </>
   )
 }
